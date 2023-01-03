5a386baae7d18c32d58a07eb67e2e186466ff8d0
Ankit <ankitsaini8847@gmail.com>
2023-01-03 19:07:57 +0530

    firstdatabse

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const {mongourl} = require('./secret.js');
+require('./models/schema.js')
+const userRouter = require('./routes/authy.js');
+
+const port = 3000;
+
+console.log("app.js loaded");
+console.log(mongourl);
+
+
+// connect mongodb:
+mongoose.connect(mongourl) 
+
+// true case - established
+mongoose.connection.on("connected" , ()=>{console.log("connected to mongo")})
+
+
+// false case - not established
+mongoose.connection.on("error" , (err)=>{console.log("error connecting to mongo", err)})
+
+
+
+app.use(express.json());
+
+app.use("/api/auth",userRouter);
+
+
+app.listen(port, () => {
+  console.log(`Authy app listening at http://localhost:${port}`)
+})
